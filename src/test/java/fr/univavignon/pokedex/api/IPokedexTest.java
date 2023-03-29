97c9c62342807c2406fe0a49430410b3ad616508
AbdouNiang <abdou.niang@alumni.univ-avignon.fr>
2023-03-29 18:12:50 +0200

    Creation des classes qui implementant IPokedex et IPokedexFactory et tester les methodes IPokedexTest qui sont echoués car pas d'implementation

--- src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
@@ -26,24 +26,45 @@ public class IPokedexTest {
 
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp()  {
+        pokedex = new Pokedex();
+        pokemon0 = new Pokemon(0,"Bulbizarre", 126,126,90,613,64,4000,4,56);
+        pokemon133 = new Pokemon(133,"Aquali", 186,168,260,2729,202,5000,4,100);
+        when(pokemon0.getIndex()).thenReturn(0);
+        when(pokemon133.getIndex()).thenReturn(133);
+
+
 
     }
 
     @Test
     public void size() {
+        int result = pokedex.size();
+        pokedex.addPokemon(pokemon0);
+        assertEquals(result + 1, pokedex.size());
     }
 
     @Test
     public void addPokemon() {
+        int result = pokedex.addPokemon(pokemon0);
+        assertEquals(pokemon0.getIndex(), result);
+        result = pokedex.addPokemon(pokemon133);
+        assertEquals(pokemon133.getIndex(), result);
     }
 
     @Test
-    public void getPokemon() {
+    public void getPokemon() throws PokedexException {
+        Pokemon result = pokedex.getPokemon(133);
+        assertEquals(pokemon133, result);
     }
 
+
     @Test
-    public void getPokemons() {
+    public void getPokemons() throws PokedexException {
+        pokedex.getPokemon(-1);
+        assertThrows(PokedexException.class, () -> {
+
+        });
     }
 
     @Test
